d364219794e75fdd6a56ee01312a271016acc454
ablueblaze <60662330+ablueblaze@users.noreply.github.com>
2021-05-05 18:34:43 -0500

    Created empty functions for future operations. Found a way to get a number from the current code. Found how to display a number from the dom. Next is to reduce the number of eventListeners.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function add(a, b){
     return a + b;
 }
@@ -14,6 +15,70 @@ function divide(a, b){
     return a / b;
 }
 
+function percent(){
+
+}
+
+function posNeg(){
+
+}
+
+function decimal(){
+
+}
+
+function clear(){
+
+}
+
+function equal(){
+
+}
+
 function operate(firstNum, secondNum, operation){
     return operation(firstNum, secondNum);
 }
+
+function hasId(element) {
+    return typeof element.id != 'undefined';
+}
+
+// Utilizing querySelectorAll allows for the number variable to scoop up all of the numbers into one.
+let number = document.querySelectorAll(".number");
+let operation = document.querySelectorAll(".operation");
+
+
+
+// Using forEach is like putting a "onClick" function onto every number button.
+number.forEach((num) => {
+    num.addEventListener("click", (event) => {
+        // Using Number() to grab the value and make it into... A NUMBER!!!
+        console.log(Number(event.target.value));
+        
+        if (event.target.id == 'undefined'){
+            console.log("yep")
+        } 
+    })
+});
+
+operation.forEach((op) => {
+    op.addEventListener("click", (event) => {
+        console.log(event.target.id)
+
+        if (event.target.id == 'undefined') {
+            console.log("yep")
+        } else {
+            console.log("nope")
+        }
+    })
+});
+
+function display(number){
+    let display = document.querySelector("#display")
+    let num = document.createTextNode(number)
+    display.appendChild(num);
+}
+
+
+display(5)
+console.log("test");
