6ca3aed7c8b9d8d57555086d823f722f8521997a
ablueblaze <60662330+ablueblaze@users.noreply.github.com>
2021-05-07 18:04:15 -0500

    Implemented a simple solution for the display that worked.

--- script.js
+++ script.js
@@ -39,13 +39,16 @@ function operate(firstNum, secondNum, operation){
     return operation(firstNum, secondNum);
 }
 
-function hasId(element) {
-    return typeof element.id != 'undefined';
-}
 
 // Utilizing querySelectorAll allows for the number variable to scoop up all of the numbers into one.
 let button = document.querySelectorAll("button");
 
+
+function display(number){
+    let display = document.querySelector("#display")
+    let num = number;
+    display.firstChild.nodeValue = num;
+}
 
 
 // Using forEach is like putting a "onClick" function onto every number button.
@@ -54,18 +57,13 @@ button.forEach((btn) => {
         if (event.target.className == 'number'){
             // Using Number() to grab the value and make it into... A NUMBER!!!
             console.log(Number(event.target.value));
+            display(Number(event.target.value));
         } else if (event.target.className == "operation"){
             console.log(event.target.id);
         }
     })
 });
 
-
-function display(number){
-    let display = document.querySelector("#display")
-    let num = document.createTextNode(number)
-    display.appendChild(num);
-}
 
 
 display(5)
