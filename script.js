3238d4ed5946e2e5a76151ae2c391e4846c9d67d
ablueblaze <60662330+ablueblaze@users.noreply.github.com>
2021-05-08 19:20:49 -0500

    Implimented a function to get the calculations to run.

--- script.js
+++ script.js
@@ -36,13 +36,14 @@ function equal(){
 }
 
 function operate(firstNum, secondNum, operation){
-    return operation(firstNum, secondNum);
+    return this[operation](Number(firstNum), Number(secondNum));
 }
 
 
 // Utilizing querySelectorAll allows for the number variable to scoop up all of the numbers into one.
 let button = document.querySelectorAll("button");
-
+let holdNum = '0';
+let holdOperator = '';
 
 function display(number){
     let display = document.querySelector("#display")
@@ -50,6 +51,20 @@ function display(number){
     display.firstChild.nodeValue = num;
 }
 
+function heldVariables(btnClass){
+    switch (btnClass){
+        case "number" :
+            if (holdNum == '0'){
+                holdNum = event.target.value;
+            } else{
+                holdNum = operate(holdNum, event.target.value, holdOperator);
+            }
+        case "operation" :
+            holdOperator = event.target.value;
+    }
+    
+
+}
 
 // Using forEach is like putting a "onClick" function onto every number button.
 button.forEach((btn) => {
@@ -57,14 +72,17 @@ button.forEach((btn) => {
         if (event.target.className == 'number'){
             // Using Number() to grab the value and make it into... A NUMBER!!!
             console.log(Number(event.target.value));
-            display(Number(event.target.value));
+            heldVariables(event.target.className)
+            display(holdNum)
         } else if (event.target.className == "operation"){
-            console.log(event.target.id);
+            console.log(event.target.value);
+            holdOperator = event.target.value
         }
     })
 });
 
 
 
-display(5)
+
+display(holdNum)
 console.log("test");
